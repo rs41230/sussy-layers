4eabab2f37ae96e0b0cbd00d5ba78e2da7a23b8f
rs41230 <homesareteachers@gmail.com>
2025-09-28 21:46:57 +0800

    Update alephlayer.js
    uttp

--- js/classes/alephlayer.js
+++ js/classes/alephlayer.js
@@ -14,25 +14,25 @@ class AlephLayer
             alephBoost: new AlephUpgrade("Gain more tasks based on the log(tasks) you have",
                 level => new Decimal(1e6).pow(Decimal.pow(1.5, level)),
                 level => new Decimal(1).add(Decimal.max(0, game.alephLayer.aleph).add(1).log10().mul(level).mul(0.05)).pow(2.5)),
-            deltaBoost: new AlephUpgrade("Gain more 🗡",
-                level => Decimal.pow(1e5, level).mul(1e3),
-                level => Decimal.pow(10, level), {
-                    maxLevel: 4
+            deltaBoost: new AlephUpgrade("Gain more SSSSSSSSSSSSSSSSSSSSSSSSSSLLLLLLLLLLLLLLLLLLLLLIIIIIIIIIIIIIIIIIICCCCCCCCCCCCCCCCCCCEEEEEEEEEEEEEEEEEEEEEEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CCCCCCCCCCCCCCCCCCCCCCCCCUUUUUUUUUUUUUUUUUUUUUUUUUUUTTTTTTTTTTTTTTTTTTTTTTTTTTTTT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+                level => Decimal.pow(1e5, level).mul(1.01),
+                level => Decimal.pow(1e10, level), {
+                    maxLevel: 1e10
                 }),
             powerGenerators: new AlephUpgrade("All Power Generators on every Layer are stronger",
                 level => Utils.createValueDilation(Decimal.pow(1e5, Decimal.pow(level, 1.5)).mul(1e20), 0.001),
-                level => Decimal.pow(1.5, level)),
+                level => Decimal.pow(2, level)),
             prestigeNoPowerBoost: new AlephUpgrade("Increase Prestige Reward on all Layers that don't have Power Generators",
-                level => Decimal.pow(1e8, level).mul(1e22),
-                level => Decimal.pow(2, level), {
-                    maxLevel: 3
+                level => Decimal.pow(1e8, level).mul(1e2),
+                level => Decimal.pow(20, level), {
+                    maxLevel: 1000
                 }),
             alephBoost2: new AlephUpgrade("Gain more tasks based on the log(log(○)) you have",
                 level => Utils.createValueDilation(Decimal.pow(1e30, level).mul(1e100), 0.01),
                 level => game.layers[0] ? Decimal.pow(new Decimal(1.1).add(level.mul(0.1)), Decimal.max(0, game.layers[0].resource).add(1).log10().add(1).log10()) : new Decimal(1)),
-            betterBetaFormula: new AlephUpgrade("The ☛ Prestige Formula is better",
+            betterBetaFormula: new AlephUpgrade("The MIDDLE FINGER Prestige Formula is better",
                 level => new Decimal(1e90),
-                level => new Decimal(1).add(level.mul(0.12)), {
+                level => new Decimal(1).add(level.mul(0.5)), {
                     maxLevel: 1,
                     getEffectDisplay: effectDisplayTemplates.numberStandard(2, "^")
                 }),
@@ -40,9 +40,9 @@ class AlephLayer
                 level => Utils.createValueDilation(Decimal.pow(1e30, level).mul(1e100), 0.005, new Decimal("1e650")),
                 level => Decimal.pow(1.6, level)),
             layerExponentialBoost: new AlephUpgrade("Increase the exponential difference of boosts between layers, resulting in a large boost!",
-                level => level.lt(2) ? new Decimal([1e125, 1e210][level.toNumber()]) : Decimal.dInf,
-                level => [22, 25, 27][level.toNumber()], {
-                    maxLevel: 2,
+                level => level.lt(2) ? new Decimal([1e125, 1e210, 1e400, 1e770, 1e1700, 1e1800, 1e2000, 1e10000, 1e100000, 1e1000000987654321000123456789][level.toNumber()]) : Decimal.dInf,
+                level => [22, 25, 27, 30, 99, 999, 9e9, 9e99, 9e9999, 9e9999999999][level.toNumber()], {
+                    maxLevel: 9,
                     getEffectDisplay: effectDisplayTemplates.numberStandard(0, "")
                 })
         };
@@ -65,7 +65,7 @@ class AlephLayer
     {
         if(functions.maxLayerUnlocked() < 3) return new Decimal(0);
         if(game.layers[3].timesReset === 0) return new Decimal(0);
-        return Decimal.pow(10, Math.max(0, functions.maxLayerUnlocked() - 3));
+        return Decimal.pow(0.01, Math.max(0, functions.maxLayerUnlocked() - 3));
     }
 
     maxAll()
